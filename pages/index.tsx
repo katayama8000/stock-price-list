d8ac59e162b3598071a97e4f5696b68bb5d37146
katayama8000 <tattu.0310@gmail.com>
2023-03-26 15:30:32 +0900

    feat: update

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,94 +8,67 @@ import {
   Text,
 } from '@chakra-ui/react';
 import Head from 'next/head';
-import { useState } from 'react';
+import { FC, useState } from 'react';
 
-type Stock = {
+type TStock = {
   stockPrice: number;
   dividend: number;
 };
 
-type CompanyData = {
-  name: string;
-  dividendYield: number;
-  stockPrice: number;
-  dividend: number;
-  desiredYield: number;
-};
+type TCompanyData = {
+  companyName: string;
+  divYield: number; // 利回り
+  desiredYield: number; // 希望配当金
+  stockCode: string; // 株コード
+} & TStock;
 
 const STOCK_CODE = '8591';
 const HELLO_CODE = '5023';
 
-const companyData: CompanyData[] = [
+const dummy: TCompanyData[] = [
   {
-    name: 'ABC社',
-    dividendYield: 2.5,
-    stockPrice: 1000,
-    dividend: 25,
-    desiredYield: 3.0,
+    companyName: '東京海上日動火災保険',
+    stockPrice: 10000,
+    dividend: 500,
+    divYield: 0, // 配当金/株価
+    desiredYield: 2,
+    stockCode: '8591',
   },
   {
-    name: 'XYZ社',
-    dividendYield: 1.8,
-    stockPrice: 800,
-    dividend: 14.4,
-    desiredYield: 2.0,
-  },
-  {
-    name: 'DEF社',
-    dividendYield: 3.2,
-    stockPrice: 1200,
-    dividend: 38.4,
-    desiredYield: 3.0,
-  },
-  {
-    name: 'ABC社',
-    dividendYield: 2.5,
-    stockPrice: 1000,
-    dividend: 25,
-    desiredYield: 3.0,
-  },
-  {
-    name: 'XYZ社',
-    dividendYield: 1.8,
-    stockPrice: 800,
-    dividend: 14.4,
-    desiredYield: 2.0,
-  },
-  {
-    name: 'DEF社',
-    dividendYield: 3.2,
-    stockPrice: 1200,
-    dividend: 38.4,
-    desiredYield: 3.0,
+    companyName: '三菱ＵＦＪフィナンシャル',
+    stockPrice: 10000,
+    dividend: 100,
+    divYield: 0, // 配当金/株価
+    desiredYield: 2,
+    stockCode: '8306',
   },
 ];
 
 export default function Home() {
-  const [stockData, setStockData] = useState<Stock | null>(null);
-  const [isLoadingStockData, setIsLoadingStockData] = useState<boolean>(false);
+  const [stock, setStock] = useState<TStock | null>(null);
+  const [isLoadingStock, setIsLoadingStock] = useState<boolean>(false);
 
-  const handleFetchStockData = async () => {
-    setIsLoadingStockData(true);
+  const handleFetchStock = async () => {
+    setIsLoadingStock(true);
     const res = await fetch(`/api/stock?code=${STOCK_CODE}`);
-    const { stockPrice, dividend }: Stock = await res.json();
-    setStockData({ stockPrice, dividend });
-    setIsLoadingStockData(false);
+    const { stockPrice, dividend }: TStock = await res.json();
+    setStock({ stockPrice, dividend });
+    setIsLoadingStock(false);
   };
 
-  const handleFetchHelloData = async () => {
+  const handleFetchHello = async () => {
     const res = await fetch(`/api/hello?code=${HELLO_CODE}`);
     const data = await res.json();
     console.log(data);
   };
 
   const [companyDataState, setCompanyDataState] =
-    useState<CompanyData[]>(companyData);
+    useState<TCompanyData[]>(dummy);
 
   const handleUpdateData = (index: number) => {
     setCompanyDataState((prevState) => {
       const newData = [...prevState];
-      newData[index].dividendYield = Math.round(Math.random() * 5 * 10) / 10;
+      newData[index].divYield = Math.round(Math.random() * 5 * 10) / 10;
       newData[index].stockPrice = Math.round(Math.random() * 1000);
       return newData;
     });
@@ -109,56 +82,61 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box minH="100vh">
-        {/* <Center bgColor={'white'}> */}
-        {companyData.map((data, index) => (
+        <Button>登録</Button>
+        {companyDataState.map((data, index) => (
           <StockCard
-            key={data.name}
-            data={data}
+            key={data.companyName}
+            {...data}
             onUpdateData={() => handleUpdateData(index)}
           />
         ))}
-        {/* </Center> */}
       </Box>
     </>
   );
 }
 
-type CardProps = {
-  data: CompanyData;
+type Props = {
   onUpdateData: () => void;
-};
+} & TCompanyData;
 
-export const StockCard = ({ data, onUpdateData }: CardProps) => {
-  const { name, dividendYield, stockPrice, dividend, desiredYield } = data;
-
+export const StockCard: FC<Props> = ({
+  onUpdateData,
+  companyName,
+  stockPrice,
+  dividend,
+  desiredYield,
+  stockCode,
+}) => {
+  const divYield: number = Math.round((dividend / stockPrice) * 100 * 10) / 10;
   return (
     <Center>
       <Box
-        bgColor={
-          data.dividendYield >= data.desiredYield ? 'green.100' : 'red.100'
-        }
+        bgColor={divYield >= desiredYield ? 'green.100' : 'red.100'}
         rounded="md"
         shadow="md"
         width="80"
         p="4"
         m="2"
       >
-        <Heading size="md">{name}</Heading>
+        <Heading size="md">{companyName}</Heading>
         <Stack divider={<StackDivider />} spacing="2" pt="2">
           <Text pt="2" fontSize="sm">
-            Dividend Yield : {dividendYield}%
+            現在の株価 : {stockPrice}円 | 希望株価 :
+            {(stockPrice * divYield) / desiredYield}円
           </Text>
           <Text pt="2" fontSize="sm">
-            Stock Price : {stockPrice}円
-          </Text>
-
-          <Text pt="2" fontSize="sm">
-            Dividend : {dividend}円
+            利回り : {divYield}% | 希望利回り : {desiredYield}%
           </Text>
           <Text pt="2" fontSize="sm">
-            Desired Yield : {desiredYield}%
+            配当金 : {dividend}円
           </Text>
-          <Button onClick={onUpdateData}>Update Data</Button>
+          <Button
+            onClick={onUpdateData}
+            colorScheme={divYield >= desiredYield ? 'green' : 'red'}
+            variant="outline"
+          >
+            更新
+          </Button>
         </Stack>
       </Box>
     </Center>
