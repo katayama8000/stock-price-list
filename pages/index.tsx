dbcc5152bea6082b7a9f10a1f507a22eb5dee70f
katayama8000 <tattu.0310@gmail.com>
2023-03-26 22:18:11 +0900

    feat: RHF

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,34 +23,42 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  FormErrorMessage,
+  PinInput,
+  PinInputField,
 } from '@chakra-ui/react';
 import Head from 'next/head';
-import { type FC, useState, useRef } from 'react';
+import { type FC, useState, useRef, useEffect } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 
 type TStock = {
   stockPrice: number;
   dividend: number;
 };
 
-type TCompanyData = {
-  companyName: string;
+type TCompany = {
+  brand: string;
   desiredYield: number; // 希望配当金
   stockCode: string; // 株コード
-} & TStock;
+};
+
+type TStockCard = TStock & TCompany;
 
 const STOCK_CODE = '8591';
 const HELLO_CODE = '5023';
 
-const dummy: TCompanyData[] = [
+const dummy: TStockCard[] = [
   {
-    companyName: '東京海上日動火災保険',
+    brand: '東京海上日動火災保険',
     stockPrice: 4000,
     dividend: 222,
     desiredYield: 2,
     stockCode: '8591',
   },
   {
-    companyName: '三菱ＵＦＪフィナンシャル',
+    brand: '三菱ＵＦＪフィナンシャル',
     stockPrice: 13050,
     dividend: 100,
     desiredYield: 2,
@@ -76,8 +84,7 @@ export default function Home() {
     console.log(data);
   };
 
-  const [companyDataState, setCompanyDataState] =
-    useState<TCompanyData[]>(dummy);
+  const [companyDataState, setCompanyDataState] = useState<TStockCard[]>(dummy);
 
   const handleUpdateData = (index: number) => {
     setCompanyDataState((prevState) => {
@@ -98,7 +105,7 @@ export default function Home() {
         <RegisterModal />
         {companyDataState.map((data, index) => (
           <StockCard
-            key={data.companyName}
+            key={data.brand}
             {...data}
             onUpdateData={() => handleUpdateData(index)}
           />
@@ -110,11 +117,11 @@ export default function Home() {
 
 type Props = {
   onUpdateData: () => void;
-} & TCompanyData;
+} & TStockCard;
 
 export const StockCard: FC<Props> = ({
   onUpdateData,
-  companyName,
+  brand,
   stockPrice,
   dividend,
   desiredYield,
@@ -134,7 +141,7 @@ export const StockCard: FC<Props> = ({
         p="4"
         m="2"
       >
-        <Heading size="md">{companyName}</Heading>
+        <Heading size="md">{brand}</Heading>
         <Stack divider={<StackDivider />} spacing="2" pt="2">
           <Text pt="2" fontSize="sm">
             現在の株価 : {stockPrice}円 | 希望株価 :{desiredStockPrice}円
@@ -173,8 +180,32 @@ export const StockCard: FC<Props> = ({
 };
 
 const RegisterModal: FC = () => {
+  const schema = z.object({
+    brand: z.string().nonempty('銘柄を入力してください'),
+    stockCode: z.string(),
+    desiredYield: z.string().transform((val) => Number(val)),
+  });
+
+  type TSchema = z.infer<typeof schema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<TSchema>({
+    resolver: zodResolver(schema),
+  });
+
   const { isOpen, onOpen, onClose } = useDisclosure();
   const initialRef = useRef(null);
+  const onSubmit = async (data: TSchema) => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
 
   return (
     <>
@@ -184,39 +215,80 @@ const RegisterModal: FC = () => {
 
       <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>新規銘柄登録</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>会社名</FormLabel>
-              <Input placeholder="トヨタ自動車" />
-            </FormControl>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <ModalContent>
+            <ModalHeader>新規銘柄登録</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl id="brand" isInvalid={Boolean(errors.brand)}>
+                <FormLabel>銘柄</FormLabel>
+                <Input placeholder="トヨタ自動車" {...register('brand')} />
+                <FormErrorMessage>
+                  {errors.brand && errors.brand?.message}
+                </FormErrorMessage>
+              </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>銘柄コード</FormLabel>
-              <Input placeholder="1234" />
-            </FormControl>
+              <FormControl
+                mt={4}
+                id="stockCode"
+                isInvalid={Boolean(errors.stockCode)}
+              >
+                <FormLabel>銘柄コード</FormLabel>
+                <Controller
+                  name="stockCode"
+                  control={control}
+                  render={({ field: { ref, ...rest } }) => (
+                    <PinInput otp {...rest}>
+                      <PinInputField />
+                      <PinInputField />
+                      <PinInputField />
+                      <PinInputField />
+                    </PinInput>
+                  )}
+                />
+                <FormErrorMessage>
+                  {errors.stockCode && errors.stockCode?.message}
+                </FormErrorMessage>
+              </FormControl>
 
-            <FormControl mt={4}>
-              <FormLabel>希望利回り(後で入力でも可)</FormLabel>
-              <NumberInput precision={1} step={0.1} placeholder={'15'}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </FormControl>
-          </ModalBody>
+              <FormControl
+                mt={4}
+                id="desiredYield"
+                isInvalid={Boolean(errors.desiredYield)}
+              >
+                <FormLabel>希望利回り(後で入力でも可)</FormLabel>
+                <Controller
+                  name="desiredYield"
+                  control={control}
+                  render={({ field }) => (
+                    <NumberInput precision={1} step={0.1}>
+                      <NumberInputField placeholder="3.5" {...field} />
+                      <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                      </NumberInputStepper>
+                    </NumberInput>
+                  )}
+                />
+                <FormErrorMessage>
+                  {errors.desiredYield && errors.desiredYield?.message}
+                </FormErrorMessage>
+              </FormControl>
+            </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
-              登録
-            </Button>
-            <Button onClick={onClose}>戻る</Button>
-          </ModalFooter>
-        </ModalContent>
+            <ModalFooter>
+              <Button
+                colorScheme="blue"
+                mr={3}
+                type="button"
+                onClick={handleSubmit(onSubmit)}
+              >
+                登録
+              </Button>
+              <Button onClick={onClose}>戻る</Button>
+            </ModalFooter>
+          </ModalContent>
+        </form>
       </Modal>
     </>
   );
