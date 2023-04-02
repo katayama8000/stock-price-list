6dff1e646fa4d8dbc354f9a9f0737d51be2ab455
katayama8000 <tattu.0310@gmail.com>
2023-04-02 13:24:08 +0900

    feat: stock → getStock

---
+++ pages/api/getStock.ts
@@ -0,0 +1,110 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { chromium, Page } from 'playwright-core';
+
+type Data = {
+  name: string;
+};
+
+type Response =
+  | {
+      stockPrice: number;
+      dividend: number;
+      brand: string;
+    }
+  | {
+      error: string;
+    };
+
+const { NEXT_PUBLIC_USERNAME, NEXT_PUBLIC_PASSWORD } = process.env;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Response>
+) {
+  console.time('playwright');
+  const { code } = req.query;
+
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  try {
+    await page.goto('https://trade.sbineomobile.co.jp/login');
+    await login(page);
+    await navigateToStockInfo(page, code);
+    const brand = await getBrand(page);
+    const currentValue = await getCurrentValue(page);
+    const dividend = await getDividend(page);
+
+    res.status(200).json({
+      stockPrice: currentValue,
+      dividend,
+      brand,
+    });
+
+    //
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'error' });
+  } finally {
+    await browser.close();
+    console.timeEnd('playwright');
+  }
+}
+
+const login = async (page: Page) => {
+  if (NEXT_PUBLIC_USERNAME === undefined || NEXT_PUBLIC_PASSWORD === undefined)
+    throw new Error('環境変数が設定されていません。');
+  await page.type('input[name="username"]', NEXT_PUBLIC_USERNAME);
+  await page.type('input[name="password"]', NEXT_PUBLIC_PASSWORD);
+  await Promise.all([
+    page.click('#neo-login-btn'),
+    page.waitForURL('https://trade.sbineomobile.co.jp/home/top'),
+  ]);
+  console.log('ログイン成功');
+};
+
+const navigateToStockInfo = async (
+  page: Page,
+  code: string | string[] | undefined
+): Promise<void> => {
+  const url = `https://trade.sbineomobile.co.jp/domestic/stockInfo/brand?securitiesCode=${code}`;
+  await Promise.all([page.goto(url), page.waitForURL(url)]);
+  console.log('ページ遷移成功');
+};
+
+const getBrand = async (page: Page): Promise<string> => {
+  const brandSelector = 'h3.name';
+  await page.waitForSelector(brandSelector);
+  const brand = await page.$eval(
+    brandSelector,
+    (el) => (el as HTMLElement).innerText
+  );
+  console.log(`銘柄名は${brand}です。`);
+  return brand;
+};
+
+const getCurrentValue = async (page: Page): Promise<number> => {
+  const currentValue: string = await page.$eval('div.label + span', (el) => {
+    return (el as HTMLElement).innerText;
+  });
+  console.log(`現在の株価は${currentValue}です。`);
+  return parseFloat(currentValue.replace(/,/g, ''));
+};
+
+const getDividend = async (page: Page): Promise<number> => {
+  const waitTime = 3000;
+  await page.click('button.showDetail');
+  await page.waitForTimeout(waitTime);
+  console.log('モーダル表示成功');
+
+  // td._text-rの中の文字列を取得する
+  const td = await page.$('td._text-r');
+  const text = await td?.textContent();
+  if (text === undefined || text === null)
+    throw new Error('配当金が取得できませんでした。');
+  console.log('現在の配当金は', text, 'です。');
+
+  return Number(text.slice(0, -1));
+};
