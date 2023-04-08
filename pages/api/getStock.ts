6972ff36f3ba33af60cd98216e8c3929f28ac14f
katayama8000 <tattu.0310@gmail.com>
2023-04-08 16:01:23 +0900

    chore: delete

--- pages/api/getStock.ts
+++ pages/api/getStock.ts
@@ -37,8 +37,6 @@ export default async function handler(
       dividend,
       brand,
     });
-
-    //
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'error' });
